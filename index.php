3076090e72a3d54ac9599882b79cfa38259f66a8
CaioTico <caiooprogramador8@gmail.com>
2024-06-03 08:15:09 -0300

    Criado um menu de navegação na página inicial

--- index.php
+++ index.php
@@ -5,10 +5,19 @@
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
     <title>Home</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
-    <link rel='stylesheet' type='text/css' media='screen' href='main.css'>
-    <script src='main.js'></script>
+    <link rel='stylesheet' type='text/css' media='screen' href='./_css/index.css'>
+    <script src='./JS/index.js'></script>
 </head>
 <body>
-    
+    <!-- Menu Hamburguer -->
+<nav class="nav-bar">
+    <div class="nav-icon">
+        <i class="fas fa-bars"></i>
+    </div>
+    <ul class="nav-links">
+        <li><a href="login.html">Login</a></li>
+        <li><a href="cadastrar.html">Cadastrar</a></li>
+    </ul>
+</nav>
 </body>
 </html>
