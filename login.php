97eef8f142ec767a07c7067f3b6c1bd27f7ac4d7
CaioTico <caiooprogramador8@gmail.com>
2024-06-10 09:04:13 -0300

    adicionado o Java script na estrutura html

--- login.php
+++ login.php
@@ -1,32 +1,45 @@
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Tela de Loguin</title>
+    <title>Tela de Login</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
     <link rel='stylesheet' type='text/css' media='screen' href='./_css/login.css'>
     <link rel="shortcut icon" href="./favicon/favicon.ico" type="image/x-icon">
-    <script src= './JS/login.js'></script>
+    <script src='./JS/login.js' defer></script>
 </head>
 
 <body>
-    <form action=''>
-            <h1>
-                LOGIN
-            </h1>
-            <p>
-                <label for='email'>E-mail:</label>
-                <input type='email' id='email'name='email' placeholder="Digite seu email:">
-            </p>
-            <p>
-                <label for='senha'>Senha:</label>
-                <input type='password' id='senha' placeholder="Digite sua senha:">
-            </p>
-            <button onclick="acessar(); return false;">
-                Enviar
-            </button>
+    <form id="formLogin">
+        <h1>LOGIN</h1>
+        <p>
+            <label for='email'>E-mail:</label>
+            <input type='email' id='email' name='email' placeholder="Digite seu email:" required>
+        </p>
+        <p>
+            <label for='senha'>Senha:</label>
+            <input type='password' id='senha' placeholder="Digite sua senha:" required>
+        </p>
+        <button type="submit">Enviar</button>
     </form>
+    <script>
+        document.addEventListener('DOMContentLoaded', function () {
+            document.getElementById('formLogin').addEventListener('submit', function (event) {
+                event.preventDefault(); // Prevent the default form submission
+
+                let email = document.getElementById('email').value;
+                let senha = document.getElementById('senha').value;
+
+                if (!email || !senha) {
+                    alert("Campos de preenchimento obrigatório. Favor preencher");
+                } else {
+                    window.location.href = "index.php";
+                }
+            });
+        });
+    </script>
 </body>
 
 </html>
